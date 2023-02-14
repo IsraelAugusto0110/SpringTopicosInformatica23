012708c35d620ef5d0ab8728e47a8b37367eff9c
IsraelAugusto0110 <israelaugustofurtadosantos@gmail.com>
2023-02-14 15:58:17 -0300

    Projeto spring para a disciplina de topicos especiais em informatica

---
+++ src/main/java/com/example/springTopicosEspeciais2301/SpringTopicosEspeciais2301Application.java
@@ -0,0 +1,13 @@
+package com.example.springTopicosEspeciais2301;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTopicosEspeciais2301Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTopicosEspeciais2301Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springTopicosEspeciais2301/SpringTopicosEspeciais2301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springTopicosEspeciais2301;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTopicosEspeciais2301ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
