b9d950fcd27bf15c759fa2abf1f4290db8b01793
IsraelAugusto0110 <israelaugustofurtadosantos@gmail.com>
2023-02-28 09:56:24 -0300

    interface

---
+++ src/main/java/com/example/springTopicosEspeciais2301/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.example.springTopicosEspeciais2301.repository;
+
+
+import com.example.springTopicosEspeciais2301.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
