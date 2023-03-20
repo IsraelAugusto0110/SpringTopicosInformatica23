b18350e49ae08267c5f935d5ad92baf254ef68f8
IsraelAugusto0110 <israelaugustofurtadosantos@gmail.com>
2023-03-20 09:00:14 -0300

    ok

--- vue-labiv20231/src/views/AboutView.vue
+++ vue-labiv20231/src/views/AboutView.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts"></script>
-
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Olá {{ nome }}!</h1>
+    <input type="text" v-model="nome" />
+    <p v-if="nome.length > 5">Texto longo</p>
+    <p v-else>Texto curto</p>
+    <p>{{ contador }} <button @click="incrementar">Incrementar</button></p>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const nome = ref('Sorvete')
+const contador = ref(1)
+function incrementar() {
+  contador.value++
+}
+</script>
