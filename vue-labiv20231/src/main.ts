2225f85caaf8616a471f28270f6011479a83207e
IsraelAugusto0110 <israelaugustofurtadosantos@gmail.com>
2023-03-20 10:32:10 -0300

    integração com backend

--- vue-labiv20231/src/main.ts
+++ vue-labiv20231/src/main.ts
@@ -4,6 +4,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:8080'
+
 import './assets/main.css'
 
 const app = createApp(App)
