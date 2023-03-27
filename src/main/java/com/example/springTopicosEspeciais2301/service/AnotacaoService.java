54526c4263158cd06853c0b2b4e5c2778ecf281f
IsraelAugusto0110 <israelaugustofurtadosantos@gmail.com>
2023-03-27 10:30:28 -0300

    ok

--- src/main/java/com/example/springTopicosEspeciais2301/service/AnotacaoService.java
+++ src/main/java/com/example/springTopicosEspeciais2301/service/AnotacaoService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,6 +25,9 @@ public class AnotacaoService implements IAnotacaoService {
         Optional <Usuario> usuario = usuarioRepository.findById(idUsuario);
         if(usuario.isEmpty()){
             throw new UsuarioNaoEncontradoException("Usuário não encontrado");
+        }
+        if (anotacao.getDataHora()  == null){
+            anotacao.setDataHora(LocalDateTime.now());
         }
         return anotacaoRepository.save(anotacao);
     }
