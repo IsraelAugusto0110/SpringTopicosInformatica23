766294d391fce940066771467a99e6b936bdc90c
IsraelAugusto0110 <israelaugustofurtadosantos@gmail.com>
2023-03-28 18:41:01 -0300

    ok

--- src/main/java/com/example/springTopicosEspeciais2301/repository/AnotacaoRepository.java
+++ src/main/java/com/example/springTopicosEspeciais2301/repository/AnotacaoRepository.java
@@ -2,6 +2,18 @@ package com.example.springTopicosEspeciais2301.repository;
 
 import com.example.springTopicosEspeciais2301.entity.Anotacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AnotacaoRepository extends JpaRepository<Anotacao, Long> {
+    public List<Anotacao> findByTextoContains(String texto);
+
+    @Query("select a from Anotacao a where a.texto like %?1%")
+    public List<Anotacao> buscarPorTexto(String texto);
+
+    public List<Anotacao> findByUsuarioNome(String nomeUsuario);
+
+    @Query("select a from Anotacao a join a.usuario u where u.nome = ?1")
+    public List<Anotacao> buscarPorNomeUsuario(String nomeUsuario);
 }
--- src/main/java/com/example/springTopicosEspeciais2301/repository/UsuarioRepository.java
+++ src/main/java/com/example/springTopicosEspeciais2301/repository/UsuarioRepository.java
@@ -5,4 +5,5 @@ import com.example.springTopicosEspeciais2301.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    public Usuario findByNome(String nome);
 }
