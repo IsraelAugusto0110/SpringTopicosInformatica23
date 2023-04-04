5c18c87fc9711f11a26e305574a56b1896180de5
ian ferreira <iantupi2@gmail.com>
2023-04-04 09:23:03 -0300

    ok

---
+++ src/main/java/com/example/springTopicosEspeciais2301/entity/Autorizacao.java
@@ -0,0 +1,54 @@
+package com.example.springTopicosEspeciais2301.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "aut_autorizacao")
+public class Autorizacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "aut_id")
+    private Long id;
+
+    @Column(name = "aut_nome")
+    private String nome;
+
+    @ManyToMany(mappedBy = "autorizacoes")
+    @JsonIgnore
+    private List<Usuario> usuarios;
+
+    public Autorizacao() {
+        // Do nothing
+    }
+
+    public Autorizacao(String nome) {
+        setNome(nome);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+}
---
+++ src/main/java/com/example/springTopicosEspeciais2301/repository/AutorizacaoRepository.java
@@ -0,0 +1,7 @@
+package com.example.springTopicosEspeciais2301.repository;
+
+import com.example.springTopicosEspeciais2301.entity.Autorizacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorizacaoRepository extends JpaRepository<Autorizacao, Long> {
+}
