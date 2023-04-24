be376a732c5d13a291dff5e9841cd77ef0cde97c
IsraelAugusto0110 <israelaugustofurtadosantos@gmail.com>
2023-04-24 18:12:53 -0300

    ok

--- ddl.sql
+++ ddl.sql
@@ -13,3 +13,6 @@ create table usr_usuario (
   primary key (usr_id),
   unique key uni_usuario_nome (usr_nome)
 );
+
+insert into usr_usuario (usr_nome, usr_senha)
+    values ('admin', '$2a$10$i3.Z8Yv1Fwl0I5SNjdCGkOTRGQjGvHjh/gMZhdc3e7LIovAklqM6C');
